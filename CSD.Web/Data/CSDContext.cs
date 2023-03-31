f70f72161ce57d3098bc52b1d78b77988ef5858a
marmax13 <marmax13@gmail.com>
2023-03-31 19:27:14 +0600

    6. В файле Program.cs устанавливаем соединение

---
+++ CSD.Web/Data/CSDContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CSD.Web.Data;
+
+public class CSDContext : DbContext
+{
+    public CSDContext(DbContextOptions options) : base(options)
+    {
+    }
+
+}
